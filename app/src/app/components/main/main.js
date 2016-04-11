1de381a1fd09ab8f4e8f33aa0afafb72594df97c
Niclas Liimatainen <niclas.liimatainen@nordsoftware.com>
2016-04-11 12:57:14 +0300

    Add talk animations to characters

--- app/src/app/components/main/main.js
+++ app/src/app/components/main/main.js
@@ -5,8 +5,69 @@ angular.module('lastenkirkko')
             $modalInstance.dismiss('cancel');
         };
     })
-    .controller('MainController', function ($scope, $log, $modal, StorageService) {
+    .controller('MainController', function ($scope, $log, $modal, StorageService, $timeout) {
         $scope.username = null;
+        $scope.isActive = false;
+
+        $scope.animateToimi = function (el, soundfile) {
+            $scope.toimiActive = $scope.toimiActive ? false : true;
+
+            if($scope.toimiActive) {
+                playSound(el, soundfile);
+
+                $scope.toimiTimeout = $timeout(function() {
+                    $scope.toimiActive = false;
+                }, 11000);
+            } else {
+                pauseSound(el);
+                $timeout.cancel($scope.toimiTimeout);
+            }
+        };
+
+        $scope.animateTuuli = function (el, soundfile) {
+            $scope.tuuliActive = $scope.tuuliActive ? false : true;
+
+            if($scope.tuuliActive) {
+                playSound(el, soundfile);
+
+                $scope.tuuliTimeout = $timeout(function() {
+                    $scope.tuuliActive = false;
+                }, 8176);
+            } else {
+                pauseSound(el);
+                $timeout.cancel($scope.tuuliTimeout);
+            }
+        };
+
+        $scope.animateToivo = function (el, soundfile) {
+            $scope.toivoActive = $scope.toivoActive ? false : true;
+
+            if($scope.toivoActive) {
+                playSound(el, soundfile);
+
+                $scope.toivoTimeout = $timeout(function() {
+                    $scope.toivoActive = false;
+                }, 6000);
+            } else {
+                pauseSound(el);
+                $timeout.cancel($scope.toivoTimeout);
+            }
+        };
+
+        $scope.animateTaito = function (el, soundfile) {
+            $scope.taitoActive = $scope.taitoActive ? false : true;
+
+            if($scope.taitoActive) {
+                playSound(el, soundfile);
+
+                $scope.taitoTimeout = $timeout(function() {
+                    $scope.taitoActive = false;
+                }, 8385);
+            } else {
+                pauseSound(el);
+                $timeout.cancel($scope.taitoTimeout);
+            }
+        };
 
         $scope.openModal = function (runningNumber) {
             $modal.open({
@@ -21,6 +82,14 @@ angular.module('lastenkirkko')
             });
         };
 
+        function pauseSound(el) {
+            el.mp3.pause();
+        }
+
+        function playSound(el,soundfile) {
+            el.mp3 = new Audio(soundfile);
+            el.mp3.play();
+        }
 
         function showLogin() {
             var modalInstance = $modal.open({
